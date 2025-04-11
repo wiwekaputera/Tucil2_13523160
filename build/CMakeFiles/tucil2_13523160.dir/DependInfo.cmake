4c6b7f358da1ee25527292bd38a274e8d6020f12
Wiweka Putera <wiwekaputera@gmail.com>
2025-04-11 15:47:41 +0700

    add build and exec for linux

---
+++ build/CMakeFiles/tucil2_13523160.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wiweka/Tucil2_13523160/src/main.cpp" "CMakeFiles/tucil2_13523160.dir/src/main.cpp.o" "gcc" "CMakeFiles/tucil2_13523160.dir/src/main.cpp.o.d"
+  "/home/wiweka/Tucil2_13523160/src/modules/errorMethods.cpp" "CMakeFiles/tucil2_13523160.dir/src/modules/errorMethods.cpp.o" "gcc" "CMakeFiles/tucil2_13523160.dir/src/modules/errorMethods.cpp.o.d"
+  "/home/wiweka/Tucil2_13523160/src/modules/gif_maker.cpp" "CMakeFiles/tucil2_13523160.dir/src/modules/gif_maker.cpp.o" "gcc" "CMakeFiles/tucil2_13523160.dir/src/modules/gif_maker.cpp.o.d"
+  "/home/wiweka/Tucil2_13523160/src/modules/imageUtils.cpp" "CMakeFiles/tucil2_13523160.dir/src/modules/imageUtils.cpp.o" "gcc" "CMakeFiles/tucil2_13523160.dir/src/modules/imageUtils.cpp.o.d"
+  "/home/wiweka/Tucil2_13523160/src/modules/quadTree.cpp" "CMakeFiles/tucil2_13523160.dir/src/modules/quadTree.cpp.o" "gcc" "CMakeFiles/tucil2_13523160.dir/src/modules/quadTree.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
